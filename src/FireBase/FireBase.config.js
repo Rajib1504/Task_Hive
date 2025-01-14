f8d3400a4a7d3b404a3750747a44e9a5a569f5de
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-01-14 10:46:12 +0530

    Auth provider set up done with the firebase installation and key is secure too

---
+++ src/FireBase/FireBase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDe0sMTj0OZ8j93l6jC6PHO5oFy08xHAMw",
+  authDomain: "taskhive-40a4a.firebaseapp.com",
+  projectId: "taskhive-40a4a",
+  storageBucket: "taskhive-40a4a.firebasestorage.app",
+  messagingSenderId: "882746283483",
+  appId: "1:882746283483:web:1a656b8a6564de5bdf43fe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { createContext } from "react";
+
+export const AuthContext = createContext(null);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const authInfo = {
+    user,
+    setUser,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
