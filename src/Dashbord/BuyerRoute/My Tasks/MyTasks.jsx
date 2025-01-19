42990308c294848e52f4a236f10358cda472c9ee
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-01-19 12:52:51 +0530

    table is ready now we will start the action work

--- src/Dashbord/BuyerRoute/My Tasks/MyTasks.jsx
+++ src/Dashbord/BuyerRoute/My Tasks/MyTasks.jsx
@@ -1,44 +1,51 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import UseAxiosSecure from "./../../../Hooks/UseAxios/UseAxiosSecure";
 import UseAuth from "./../../../Hooks/useAuth/UseAuth";
 import { FaEdit, FaTrash } from "react-icons/fa";
+import { data } from "react-router-dom";
 
 const MyTasks = () => {
   const [Buyer_details, setBuyer_details] = useState([]);
+  // console.log(Buyer_details);
   const axiosSecure = UseAxiosSecure();
   const { user } = UseAuth();
+
   useEffect(() => {
     axiosSecure
       .get(`/mytasks/${user?.email}`)
       .then((res) => {
         const Buyer_details = res.data;
-        setBuyer_details(Buyer_details);
+        const sorted_data = Buyer_details.sort(
+          (a, b) => new Date(b.deadline) - new Date(a.deadline)
+        );
+
+        setBuyer_details(sorted_data);
       })
       .catch((err) => {
         console.log(err.message);
       });
-  }, []);
+  }, [axiosSecure, user?.email]);
+
   return (
     <div>
       <h2 className="font-bold text-center my-4 text-3xl">My Tasks</h2>
-      {/* table  */}
+      {/* Table */}
       <div>
         <div className="overflow-x-auto">
           <table className="table">
-            {/* head */}
+            {/* Head */}
             <thead>
               <tr>
                 <th>
                   <label>Sl</label>
                 </th>
-                <th>Task_Tittle</th>
+                <th>Task_Title</th>
                 <th>Deadline</th>
-                <th>Action</th>
-                <th></th>
+                <th>Actions</th>
               </tr>
             </thead>
             <tbody>
-              {/* body is staring of  table  */}
+              {/* Body of the Table */}
               {Buyer_details.map((item, idx) => (
                 <tr key={item._id}>
                   <th>
@@ -53,27 +60,22 @@ const MyTasks = () => {
                       </div>
                       <div>
                         <div className="font-bold">{item.title}</div>
+                        <div className="flex items-center justify-center gap-2 ">
+                          <div className="text-sm opacity-50 badge">
+                            vacancy:{item.requiredWorkers}
+                          </div>
+                          <div className="text-sm opacity-50 badge">
+                            payment: $ {item.payableAmount}
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </td>
-                  <td>
-                    {item.deadline}
-                    <br />
-                    <span className="badge badge-ghost badge-sm">
-                      Worker need : {item.requiredWorkers}
-                    </span>
-                    <span className="badge badge-ghost badge-sm">
-                      Payble amount : {item.payableAmounts}
-                    </span>
+                  <td>Last Date: {item.deadline}</td>
+                  <td className="flex items-center gap-3">
+                    <FaEdit className="text-secondary lg:text-xl cursor-pointer" />
+                    <FaTrash className="text-red-500 lg:text-xl cursor-pointer ml-4" />
                   </td>
-                  <span>
-                    <td>
-                      <FaEdit className="text-secondary lg:text-xl"></FaEdit>
-                    </td>
-                    <td>
-                      <FaTrash className="text-red-500 lg:text-xl"></FaTrash>
-                    </td>
-                  </span>
                 </tr>
               ))}
             </tbody>
