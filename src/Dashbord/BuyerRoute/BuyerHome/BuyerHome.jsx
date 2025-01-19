f43c82a31fce7e63f6686602fccf70522a4f5df9
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-01-20 00:15:23 +0530

    buyer home work is started we show his tasks

--- src/Dashbord/BuyerRoute/BuyerHome/BuyerHome.jsx
+++ src/Dashbord/BuyerRoute/BuyerHome/BuyerHome.jsx
@@ -1,7 +1,39 @@
-import React from "react";
-
+import { use, useEffect, useState } from "react";
+import UseAuth from "./../../../Hooks/useAuth/UseAuth";
+import UseAxiosSecure from "./../../../Hooks/UseAxios/UseAxiosSecure";
 const BuyerHome = () => {
-  return <div>buyerhome</div>;
+  const { user } = UseAuth();
+  const axiosSecure = UseAxiosSecure();
+  const [Buyer_data, setBuyer_data] = useState([]);
+  useEffect(() => {
+    axiosSecure.get(`/mytasks/${user.email}`).then((res) => {
+      const data = res.data;
+      // console.log(data);
+      setBuyer_data(data);
+    });
+  }, []);
+  return (
+    <>
+      {/* buyer state section  */}
+      <div className="flex justify-around items-center">
+        <div className="flex justify-center items-center">
+          <h2 className="font-bold lg:text-2xl">
+            My Total Tasks:<span className="ml-3">{Buyer_data.length}</span>
+          </h2>
+        </div>
+        <div className="flex justify-center items-center">
+          <h2 className="font-bold lg:text-2xl">
+            Pendding Tasks:<span className="ml-3">10</span>
+          </h2>
+        </div>
+        <div className="flex justify-center items-center">
+          <h2 className="font-bold lg:text-2xl">
+            My Total Payment:<span className="ml-3">312</span>
+          </h2>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BuyerHome;
