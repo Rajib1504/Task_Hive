874d2dbdf7a6a0d97411adef8f07369e4dc0b8c0
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-01-21 14:01:03 +0530

    buyer home work is under process Buyer can see the pending tasks in his home route

--- src/Dashbord/BuyerRoute/BuyerHome/BuyerHome.jsx
+++ src/Dashbord/BuyerRoute/BuyerHome/BuyerHome.jsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import UseAuth from "./../../../Hooks/useAuth/UseAuth";
 import UseAxiosSecure from "./../../../Hooks/UseAxios/UseAxiosSecure";
 const BuyerHome = () => {
@@ -7,6 +7,9 @@ const BuyerHome = () => {
   const [Buyer_data, setBuyer_data] = useState([]);
   const [pending_tasks, setPending_tasks] = useState(0);
   const [totalPayment, setTotalPayment] = useState(0);
+  const [fullDetails, setFullDetails] = useState([]);
+  console.log("fulldetails is here", fullDetails);
+  // for state preview
   useEffect(() => {
     axiosSecure.get(`/mytasks/${user.email}`).then((res) => {
       const totalTasks = res.data;
@@ -26,6 +29,23 @@ const BuyerHome = () => {
       setTotalPayment(Payment);
     });
   }, []);
+  // for form preview
+
+  useEffect(() => {
+    axiosSecure
+      .get(`/submitData/${user.email}`)
+      .then((res) => {
+        const data = res.data;
+        setFullDetails(data);
+      })
+      .catch((err) => {
+        toast.error(err.message);
+      });
+  }, [axiosSecure, user]);
+  // logic for get the status  === pending
+  const pendingStatus = fullDetails.filter(
+    (details) => details.status === "pending"
+  );
   return (
     <>
       {/* buyer state section  */}
@@ -45,6 +65,49 @@ const BuyerHome = () => {
             My Total Payment:<span className="ml-3">${totalPayment}</span>
           </h2>
         </div>
+      </div>
+      {/* Buyer form with submition details   */}
+      <div className="overflow-x-auto mt-2 sm:mt-4">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Worker_Name</th>
+              <th>Title</th>
+              <th>Amount Color</th>
+              <th>Details</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pendingStatus.length > 0 ? (
+              pendingStatus.map((details, idx) => (
+                <tr key={details._id}>
+                  <th>{idx + 1}</th>
+                  <td>{details.worker_name}</td>
+                  <td>{details.task_title}</td>
+                  <td>{details.payable_amount}</td>
+                  <td>
+                    <button className="btn btn-sm">Submition Details</button>
+                  </td>
+                  <td>
+                    <select name="Action" id="">
+                      <option value="Approve ">Approve</option>
+                      <option value="Reject  ">Reject</option>
+                    </select>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="4" className="text-center">
+                  No Pending Data Found
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
       </div>
     </>
   );
