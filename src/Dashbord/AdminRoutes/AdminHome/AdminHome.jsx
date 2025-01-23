e036e74a5e3732525612c7a4ddca79650ce61df5
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-01-23 13:27:33 +0530

    admin can see total Worker,Buyer and total coins which in the marker

--- src/Dashbord/AdminRoutes/AdminHome/AdminHome.jsx
+++ src/Dashbord/AdminRoutes/AdminHome/AdminHome.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import UseAxiosSecure from "../../../Hooks/UseAxios/UseAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import Loading from "../../../Loading/Loading";
+import { toast } from "react-toastify";
 
 const AdminHome = () => {
-  return <div>Admin Home</div>;
+  const axiosSecure = UseAxiosSecure();
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["data", axiosSecure],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/userData");
+      return res.data;
+    },
+  });
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (error) {
+    toast.error(error);
+    return null;
+  }
+  console.log(data);
+  // total_worker
+  const totalWorker = data.filter((item) => item.role === "Worker");
+  // console.log(totalWorker);
+  // total_Buyer
+  const total_buyer = data.filter((item) => item.role === "Buyer");
+  // console.log(total_buyer);
+  // total_available_coins
+  const avalible_coins = data.reduce((total, item) => {
+    return total + item.Coins;
+  }, 0);
+  // console.log(avalible_coins);
+  return (
+    <div className="bg-base-200">
+      {/* Admin state section  */}
+      <div className="flex justify-around  items-center">
+        <div className="flex justify-center items-center">
+          <h2 className="font-bold lg:text-2xl">
+            Total Workers:
+            <span className="ml-3 text-secondary">{totalWorker.length}</span>
+          </h2>
+        </div>
+        <div className="flex justify-center items-center">
+          <h2 className="font-bold lg:text-2xl">
+            Total Buyer:
+            <span className="ml-3 text-yellow-400">{total_buyer.length}</span>
+          </h2>
+        </div>
+        <div className="flex justify-center items-center">
+          <h2 className="font-bold lg:text-2xl">
+            Total Available coins:
+            <span className="ml-3 text-green-400">{avalible_coins} $</span>
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminHome;
