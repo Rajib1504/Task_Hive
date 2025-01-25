02ce7eae96b3833bd7b4e9d73fa1706eb100509b
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-01-26 02:31:32 +0530

    worker aggregation is created now

--- src/Dashbord/Worker/Worker HOme/WorkerHome.jsx
+++ src/Dashbord/Worker/Worker HOme/WorkerHome.jsx
@@ -1,14 +1,23 @@
-import useAxiosPublic from "../../../Hooks/UseAxios/useAxiosPublic";
+import UseAxiosSecure from "../../../Hooks/UseAxios/UseAxiosSecure";
 import UseAuth from "../../../Hooks/useAuth/UseAuth";
 import { useQuery } from "@tanstack/react-query";
-
 import { toast } from "react-toastify";
 import Loading from "./../../../Loading/Loading";
 
 const WorkerHome = () => {
-  const axiosPublic = useAxiosPublic();
-  const { user } = UseAuth();
+  const axiosSecure = UseAxiosSecure();
+  const { user, loading } = UseAuth();
   console.log(user.email);
+  const { data: worker_State = {} } = useQuery({
+    queryKey: ["workerStates", user.email],
+    // enabled:
+    queryFn: async () => {
+      const res = await axiosSecure(`/worker-stats/${user.email}`);
+      return res.data;
+    },
+  });
+  console.log(worker_State);
+
   const {
     data = [],
     error,
@@ -16,45 +25,42 @@ const WorkerHome = () => {
   } = useQuery({
     queryKey: ["submition", user.email],
     queryFn: async () => {
-      const res = await axiosPublic.get(`/submitDetails/${user?.email}`);
+      const res = await axiosSecure.get(`/submitDetails/${user?.email}`);
       return res.data;
     },
   });
   if (isLoading) {
-    return <Loading />;
+    return <Loading></Loading>;
   }
   if (error) {
     toast.error("something went wrong" + error.message);
     return null;
   }
-  console.log(data);
-  // pending logic
-  const pending = data.filter((item) => item.status === "pending");
-  // console.log(pending.);
-  // total income logic is created
-  const income = data.reduce((total, item) => {
-    return item.status === "Approved" ? total + item.payable_amount : total;
-  }, 0);
-  // console.log(income);
+
   const approve = data.filter((item) => item.status === "Approved");
-  console.log(approve);
+  // console.log(approve);
   return (
     <div>
       {/* state section  */}
       <div className="flex justify-around items-center">
         <div className="flex justify-center items-center">
           <h2 className="font-bold lg:text-2xl">
-            Total Submission:<span className="ml-3">{data.length}</span>
+            Total Submission:
+            <span className="ml-3">{worker_State.totalSubmissionCount}</span>
           </h2>
         </div>
         <div className="flex justify-center items-center">
           <h2 className="font-bold lg:text-2xl">
-            Pending submissions:<span className="ml-3">{pending.length}</span>
+            Pending submissions:
+            <span className="ml-3">
+              {worker_State.totalPendingSubmissionCount}
+            </span>
           </h2>
         </div>
         <div className="flex justify-center items-center">
           <h2 className="font-bold lg:text-2xl">
-            My Total Earning:<span className="ml-3">${income}</span>
+            My Total Earning:
+            <span className="ml-3">${worker_State.totalEarning}</span>
           </h2>
         </div>
       </div>
