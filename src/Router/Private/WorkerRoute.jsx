c75ac457c67102f90a4650ec6c27addae76f4c47
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-01-26 11:50:41 +0530

    worker Route is created now worker can see his routes and do work there

--- src/Router/Private/WorkerRoute.jsx
+++ src/Router/Private/WorkerRoute.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import UseRole from "../../Hooks/UseRole/UseRole";
+import Loading from "../../Loading/Loading";
+import { Navigate } from "react-router-dom";
 
-const WorkerRoute = () => {
-  return <div></div>;
+const WorkerRoute = ({ children }) => {
+  const [role, isLoading] = UseRole();
+  console.log(role);
+  if (isLoading) return <Loading />;
+  if (role === "Worker") return children;
+  return <Navigate to="/dashbord" replace="true" />;
 };
 
 export default WorkerRoute;
