4c54509f85c7c8087c9f376cd88cdcbe284cd2c9
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-01-26 11:25:29 +0530

    buyer route has created only buyer will see this routes and can access too

--- src/Router/Private/BuyerRoute.jsx
+++ src/Router/Private/BuyerRoute.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import UseRole from "../../Hooks/UseRole/UseRole";
+import Loading from "../../Loading/Loading";
+import { Navigate } from "react-router-dom";
 
-const BuyerRoute = () => {
-  return <div></div>;
+const BuyerRoute = ({ children }) => {
+  const [role, isLoading] = UseRole();
+  // console.log(role);
+  if (isLoading) return <Loading />;
+  if (role === "Buyer") return children;
+  return <Navigate to="/dashbord" replace="true" />;
 };
 
 export default BuyerRoute;
