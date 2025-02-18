ae3c1f164127077e7688ad07fb723169d56fdb16
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-02-18 12:29:25 +0530

    main component bug is fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import Router from "./Router/Router.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "./Provider/AuthProvider.jsx";
 
+
+const queryClient =new QueryClient()
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
+    <QueryClientProvider client={queryClient}>
     <RouterProvider router={Router}></RouterProvider>
+    </QueryClientProvider>
   </AuthProvider>
 );
