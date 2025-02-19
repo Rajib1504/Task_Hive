06e7e0a2ab917c61ebc90f91dd5a2e904a688a29
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-02-19 16:59:59 +0530

    main section bug is  fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import Router from "./Router/Router.jsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "./Provider/AuthProvider.jsx";
-
-
-const queryClient =new QueryClient()
+import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <QueryClientProvider client={queryClient}>
-    <RouterProvider router={Router}></RouterProvider>
+      <RouterProvider router={Router}></RouterProvider>
+      <ToastContainer position="bottom-left" />
     </QueryClientProvider>
   </AuthProvider>
 );
